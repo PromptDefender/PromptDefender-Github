23e9b1fa1c2e2903b25672938d5149be4097ac79
dllewellyn <danieln.llewellyn@gmail.com>
2024-12-08 12:25:57 +0000

    Bump version

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ terraform {
    required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.52.0"
+      version = "4.13.0"
     }
   }
-  
+
   backend "remote" {
     organization = "PromptShield"
 
